45a6f52d834aa4c470aa453c5273c41091d83da4
chanhduypq <chanhduypq@gmail.com>
2017-10-26 23:55:27 +0700

    no message

--- app/Http/Controllers/Fontend/ProductController.php
+++ app/Http/Controllers/Fontend/ProductController.php
@@ -7,39 +7,50 @@
  */
 
 namespace App\Http\Controllers\Fontend;
+
 use App\Models\Brand;
 use App\Models\Family;
 use App\Models\Product;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+
 /**
  * Description of IndexController
  *
  * @author hoanvu
  */
-class ProductController extends Controller {
-    public function brand($brandSlug, Request $req) {
+class ProductController extends Controller
+{
+
+    public function brand($brandSlug, Request $req)
+    {
         $brand = Brand::where(['slug' => $brandSlug])->first();
-        if (empty($brand)) {
+        if (empty($brand))
+        {
             return view('fontend.errors.404');
         }
         $families = Family::where(['brandId' => $brand->id])->get();
         return view('fontend.brand.index', compact('brand'))->with('families', $families)->with('input', $req->all());
     }
 
-    public function family($brandSlug, $familySlug, Request $req) {
-        
+    public function family($brandSlug, $familySlug, Request $req)
+    {
+
         $brand = Brand::where(['slug' => $brandSlug])->first();
-        if (empty($brand)) {
+        if (empty($brand))
+        {
             return view('fontend.errors.404');
         }
         $family = Family::where(['slug' => $familySlug])->first();
-        if (empty($family)) {
+        if (empty($family))
+        {
             return view('fontend.errors.404');
         }
         $query = Product::where(['familyId' => $family->id, 'brandId' => $brand->id]);
-        if ($req->searchKey) {
-            $query->where(function($q) use ($req) {
+        if ($req->searchKey)
+        {
+            $query->where(function($q) use ($req)
+            {
                 $q->orWhere('name', 'LIKE', '%' . $req->searchKey . '%');
                 $q->orWhere('slug', 'LIKE', '%' . str_slug($req->searchKey) . '%');
             });
@@ -51,33 +62,42 @@ class ProductController extends Controller {
         return view('fontend.product.family', compact('family', 'brand'))->with('products', $products)->with('input', $req->all());
     }
 
-    public function search(Request $req){
-        if($req->brandSlug){
-        $brand = Brand::where(['slug' => $req->brandSlug])->first();
-            if (empty($brand)) {
+    public function search(Request $req)
+    {
+        if ($req->brandSlug)
+        {
+            $brand = Brand::where(['slug' => $req->brandSlug])->first();
+            if (empty($brand))
+            {
                 return view('fontend.errors.404');
             }
         }
-        $query = Product::where([]);
-        $query->where(function($q) use ($req) {
-            $q->orWhere('name', 'LIKE', '%' . $req->searchKey . '%');
-            $q->orWhere('slug', 'LIKE', '%' . str_slug($req->searchKey) . '%');
+        $query = Product::select('products.*');
+        $query->leftjoin('brands', 'brands.id', '=', 'products.brandId');
+        $query->leftjoin('family', 'family.id', '=', 'products.familyId');
+        $query->where(function($q) use ($req)
+        {
+            $q->orWhere('products.name', 'LIKE', '%' . $req->searchKey . '%');
+            $q->orWhere('products.reference', 'LIKE', '%' . $req->searchKey . '%');
+            $q->orWhere('brands.name', 'LIKE', '%' . $req->searchKey . '%');
+            $q->orWhere('family.name', 'LIKE', '%' . $req->searchKey . '%');
         });
-        
         if (!empty($brand))
-            $query->where('brandId', $brand->id);
-        
+            $query->where('products.brandId', $brand->id);
+
         $limit = $req->limit ? (int) $req->limit : 10;
         $page = $req->page ? (int) $req->page : 0;
         $products = $query->paginate($limit, ['*'], 'page', $page);
-   
-        return view('fontend.product.index',compact('brand'))->with('products',$products)->with('input', $req->all());
+
+        return view('fontend.product.index', compact('brand'))->with('products', $products)->with('input', $req->all());
     }
-    
-    public function detail($brandSlug,$familySlug,$productSlug, Request $req) {
-        
+
+    public function detail($brandSlug, $familySlug, $productSlug, Request $req)
+    {
+
         $product = Product::where(['slug' => $productSlug])->first();
-        if (empty($product)) {
+        if (empty($product))
+        {
             return view('fontend.errors.404');
         }
         return view('fontend.product.detail', compact('product'));
