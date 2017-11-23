b35ecc06ce0e7f1623ae0f35f13e0aef4a2bacb3
chanhduypq <chanhduypq@gmail.com>
2017-11-23 08:33:10 +0700

    no message

--- db/family_view.sql
+++
@@ -1,4 +0,0 @@
-CREATE VIEW family_view 
-AS 
-SELECT family.id,family.createdAt,family.`name`,family.slug,family.updatedAt,brands.`name` as brand_name
-FROM family LEFT JOIN brands ON family.brandId=brands.id 
--- db/products_view.sql
+++
@@ -1,4 +0,0 @@
-CREATE VIEW products_view 
-AS 
-SELECT products.id,products.images,brands.`name` as brand_name, family.`name` as family_name,products.reference,products.`name`,products.movementType,products.diameter,products.brandId
-FROM products LEFT JOIN brands on brands.id=products.brandId LEFT JOIN family on family.id=products.familyId
